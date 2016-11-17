0cbfc31d1f98fad1436b8c37136dc3b60e49c301
Abdullah M. Alhazmy <aalhazmy@sasmc.net>
2016-11-17 20:11:15 +0300

    - First Commit

--- DemoWatchOS/ViewController.swift
+++ DemoWatchOS/ViewController.swift
@@ -7,18 +7,52 @@
 //
 
 import UIKit
+import WatchConnectivity
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,WCSessionDelegate {
 
+    var counterData = [String]()
+    let session: WCSession? = WCSession.isSupported() ? WCSession.default() : nil
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        session?.delegate = self;
+        session?.activate()
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    public func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?)
+    {
+        print(activationState.rawValue)
+        
+    }
+    
+   
+    @available(iOS 9.0, *)
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) -> Void {
+        print(message)
+        
+    }
+    
+    
+    func sessionDidBecomeInactive(_ session: WCSession) {
+    }
+    
+    func sessionDidDeactivate(_ session: WCSession) {
+        session.activate()
+    }
+    
 
 
 }
---
+++ Watch Extension/InterfaceController.swift
@@ -0,0 +1,56 @@
+//
+//  InterfaceController.swift
+//  Watch Extension
+//
+//  Created by Abdullah Alhazmy on 11/17/16.
+//  Copyright © 2016 Alhazmy. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import WatchConnectivity
+
+class InterfaceController: WKInterfaceController,WCSessionDelegate {
+    var counter = 0
+    var session : WCSession!
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        if (WCSession.isSupported()) {
+            session = WCSession.default()
+            session.delegate = self
+            session.activate()
+        }
+
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+    @IBAction func onButtonClicked() {
+        print(counter)
+        let applicationData = ["counterValue":String(counter)]
+        session.sendMessage(applicationData, replyHandler:  nil, errorHandler: { (error) -> Void in
+            print("error")
+        })
+        counter += 1
+    }
+    
+
+    @available(iOS 9.3, *)
+    public func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?)
+    {
+    }
+    
+    
+    
+    
+
+}
